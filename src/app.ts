3e7f406b82974e24a561c9b7331aecc24005475e
Murat Ozcan <muratkerem@gmail.com>
2023-08-01 13:24:19 -0500

    first commit

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ export const setupApp = async () => {
 
   const opticUrl = {
     "x-optic-url":
-      "https://app.useoptic.com/organizations/32613bcd-704e-4661-85f0-7b3d75613fb0/apis/Ru2Me4G-2nIro-cj4Bbib",
+      "https://app.useoptic.com/organizations/68c60dd6-70a3-475b-97cd-8afe39b4df14/apis/Ru2Me4G-2nIro-cj4Bbib",
   };
 
   await app.register(openapi, {
