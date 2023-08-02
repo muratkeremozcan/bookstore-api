a0a8dbb8ed6aff77e479380903a1c4bd838ce360
Murat Ozcan <muratkerem@gmail.com>
2023-08-02 10:27:20 -0500

    test without an optic url

--- src/app.ts
+++ src/app.ts
@@ -5,11 +5,14 @@ import { registerRoutes } from "./routes";
 export const setupApp = async () => {
   const app = Fastify();
 
+<<<<<<< Updated upstream
   const opticUrl = {
     "x-optic-url":
       "https://app.useoptic.com/organizations/32613bcd-704e-4661-85f0-7b3d75613fb0/apis/Ru2Me4G-2nIro-cj4Bbib",
   };
 
+=======
+>>>>>>> Stashed changes
   await app.register(openapi, {
     openapi: {
       openapi: "3.1.3",
@@ -21,7 +24,6 @@ export const setupApp = async () => {
       servers: [
         { url: "https://api.bookstore.com", description: "Production server" },
       ],
-      ...opticUrl,
     },
   });
 
